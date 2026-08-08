e4ebfe24d03b9f02b521d71726b49c7789be2f2c
Mobinafshari <mma137813781378@gmail.com>
2026-08-08 19:49:10 +0330

    feat: working on data structures

--- basics/ds/main.go
+++ basics/ds/main.go
@@ -3,17 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"hashem", "saber"}
-	names = append(names, "jasem")
-	for index := 0; index < len(names); index++ {
-		names[index] = "Mr" + " " + names[index]
 
+	studentGrades := map[string]int{
+		"Naser": 20,
+		"Jasem": 66,
 	}
-	fmt.Printf("%+v\n", names)
-	items := make([]int, 3, 4)
-	items[2] = 122
-	items = append(items, 4)
-	fmt.Printf("%+v", "cap: %d\n", items, cap(items))
+	studentGrades["Naser"] = 99
+
+	naser, ok := studentGrades["Naser"]
+	if !ok {
+		return
+	}
+	fmt.Printf("Naser: %+v\n", naser)
+
+	if hashem, ok := studentGrades["Hashem"]; ok {
+		fmt.Printf("Hashem: %+v\n", hashem)
+	}
+
+	delete(studentGrades, "Naser")
+
+	classes := make(map[int]string)
+	classes[22] = "Collage"
+
+	fmt.Printf("%+v\n", classes)
 
 }
 
@@ -33,3 +45,15 @@ func main() {
 
 // 	var matrix [2][3]int
 // 	fmt.Printf("%+v\n", matrix)
+
+// names := []string{"hashem", "saber"}
+// names = append(names, "jasem")
+// for index := 0; index < len(names); index++ {
+// 	names[index] = "Mr" + " " + names[index]
+
+// }
+// fmt.Printf("%+v\n", names)
+// items := make([]int, 3, 4)
+// items[2] = 122
+// items = append(items, 4)
+// fmt.Printf("%+v", "cap: %d\n", items, cap(items))
